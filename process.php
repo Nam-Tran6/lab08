15390b948d7f4c19b1b3bcc5c4143784a7c96b9d
Kha Nam Tran <104555355@student.swin.edu>
2025-10-08 23:10:00 +1100

    hotfix

--- process.php
+++ process.php
@@ -6,10 +6,13 @@
     //Records inputted password for PHP
     $password = $_POST["password"];
 
-    //Username is hard
-    if ($username =="Nam" && $password =="Nam104555355") {
+    //Records hidden values for comparisons
+    $secretname = $_POST["secretname"];
+    $secretpass = $_POST["secretvalue"];
+
+    if ($username == $secretname && $password == $secretvalue) {
         $_SESSION["user"] = $username;
-        header("Location:welcome.php")
+        header("Location:welcome.php");
     } else {
         echo "Invalid login. <a href='login.html'>Try again</a>";
     }
