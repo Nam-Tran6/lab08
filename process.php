13b103bef1a96bbf4bc16916e54279c1dd93c7b0
Kha Nam Tran <104555355@student.swin.edu>
2025-10-08 22:51:39 +1100

    php code test

--- process.php
+++ process.php
@@ -1,3 +1,16 @@
 <?php
+    // Sets up the Database
+    session_start();
+    //Records inputted username for PHP
+    $username = $_POST["username"];
+    //Records inputted password for PHP
+    $password = $_POST["password"];
 
+    //Username is hard
+    if ($username =="Nam" && $password =="Nam104555355") {
+        $_SESSION["user"] = $username;
+        header("Location:welcome.php")
+    } else {
+        echo "Invalid login. <a href='login.html'>Try again</a>";
+    }
 ?>
